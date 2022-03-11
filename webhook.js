56fa61e36bccfa2397f957667d8999a1fec2104a
ikazuPs <96538904+ikazuPs@users.noreply.github.com>
2022-03-11 20:00:03 +0700

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const webhook = new Discord.WebhookClient({url: "https://discord.com/api/webhooks/951824194310647848/1P8bPmFGbbELX2WgkUDi2MTE6g7AiNFZnDRM-FcJy5shK6BF2V-6gko88W9vTCE_QsVe"});
-//https://discord.com/api/webhooks/951824194310647848/1P8bPmFGbbELX2WgkUDi2MTE6g7AiNFZnDRM-FcJy5shK6BF2V-6gko88W9vTCE_QsVe
-webhook.send("Sended Webhook");
+const webhook = new Discord.WebhookClient({url: "Input Discord Webhook Link"});
+webhook.send("INPUT YOUR MESSAGE HERE");
 console.log("Sended Webhook");
